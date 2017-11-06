13d03ec641db2d281512f134a5c21a63e23a85f5
David Ko <david.ko@pvtmethod.com>
2017-11-06 17:03:09 -0500

    Connect to postgres

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 
-	"github.com/lib/pq"
+	_ "github.com/lib/pq"
 	"gopkg.in/labstack/echo.v3"
 )
 
@@ -13,8 +15,22 @@ func Home(ctx echo.Context) error {
 }
 
 func main() {
-	e := echo.New()
-	e.GET("/", Home)
-
-	e.Logger.Fatal(e.Start(":1323"))
+	// e := echo.New()
+	// e.GET("/", Home)
+	//
+	// e.Logger.Fatal(e.Start(":1323"))
+	echo.New()
+	connStr := "user=t13 dbname=t13_web_dev sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := db.Query("select * from rings limit 10")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cols, _ := rows.Columns()
+	for _, colName := range cols {
+		fmt.Println(colName)
+	}
 }
