2bc111304894ee14a1e6d1cba703aa1fba17b333
David Ko <david.ko@damncarousel.com>
2017-11-07 07:45:47 -0500

    Adds homepage templates with table display [Fixes #1] (#5)

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"html/template"
+	"io"
 	"log"
 	"net/http"
 
@@ -10,26 +11,45 @@ import (
 	"gopkg.in/labstack/echo.v3"
 )
 
-func Home(ctx echo.Context) error {
-	return ctx.String(http.StatusOK, "Hello, world!")
+const (
+	connStr = "user=t13 dbname=t13_web_dev sslmode=disable"
+)
+
+type MyRenderer struct {
+	templates *template.Template
 }
 
-func main() {
-	// e := echo.New()
-	// e.GET("/", Home)
-	//
-	// e.Logger.Fatal(e.Start(":1323"))
-	echo.New()
-	connStr := "user=t13 dbname=t13_web_dev sslmode=disable"
+func (r *MyRenderer) Render(w io.Writer, templateName string, data interface{}, ctx echo.Context) error {
+	return r.templates.ExecuteTemplate(w, templateName, data)
+}
+
+func Home(ctx echo.Context) error {
 	db, err := sql.Open("postgres", connStr)
+	var tableNames []string
 	if err != nil {
 		log.Fatal(err)
 	}
 	rows, err := db.Query("select table_name from information_schema.tables where table_schema = 'public' and table_type = 'BASE TABLE'")
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer rows.Close()
 	for rows.Next() {
 		var table_name string
 		rows.Scan(&table_name)
-		fmt.Println(table_name)
+		tableNames = append(tableNames, table_name)
 	}
+	return ctx.Render(http.StatusOK, "home", tableNames)
+}
+
+func main() {
+	e := echo.New()
+	templates := template.Must(template.ParseGlob("src/templates/*.html"))
+	e.Renderer = &MyRenderer{
+		templates: templates,
+	}
+	e.GET("/", Home)
+
+	e.Logger.Fatal(e.Start(":1323"))
+
 }
