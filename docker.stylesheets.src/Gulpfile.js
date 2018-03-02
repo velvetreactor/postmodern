59cfb887cc13c4c6452604c975e7c21f92274193
David Ko <david.ko@velvetreactor.com>
2018-03-02 11:33:39 -0500

    Finished stylesheets container

--- docker.stylesheets.src/Gulpfile.js
+++ docker.stylesheets.src/Gulpfile.js
@@ -3,12 +3,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
-gulp.task('sass', () => {
+gulp.task('default', () => {
   return gulp.src('src/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('sass:watch', () => {
-  gulp.watch('src/*.scss', ['sass']);
+  gulp.watch('src/*.scss', ['default']);
 });
