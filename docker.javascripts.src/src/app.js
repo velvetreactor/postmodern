311bd2dc88f17e39c56adf9d0832c010d339aa8a
David Ko <david.ko@velvetreactor.com>
2018-03-02 15:51:06 -0500

    Move javascript compilation into docker container

--- docker.javascripts.src/src/app.js
+++ docker.javascripts.src/src/app.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-import App from './javascripts/App.jsx';
-import masterReducer from './javascripts/reducers';
+import App from './components/App.jsx';
+import masterReducer from './components/reducers';
 
 const store = createStore(masterReducer);
 
--- docker.javascripts.src/webpack.config.js
+++ docker.javascripts.src/webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  context: __dirname + '/src',
-  entry: './app',
+  context: __dirname + '/src/',
+  entry: './app.js',
   output: {
-    path: __dirname + '/dist/javascripts',
+    path: __dirname + '/dist',
     filename: 'bundle.js'
   },
   watch: true,
