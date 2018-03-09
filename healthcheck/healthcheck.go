e02c0f9a10da2dcf6d1f3a53e2b63bc34c3f11d9
David Ko <david.ko@velvetreactor.com>
2018-03-09 06:41:47 -0500

    Postgres healthcheck

--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -2,30 +2,24 @@ package healthcheck
 
 import (
 	"database/sql"
-	"fmt"
 	"net/http"
 
 	_ "github.com/lib/pq"
 	"gopkg.in/labstack/echo.v3"
 )
 
-type Healthchecker struct {
-	ConnStr string
+type HealthchecksCtrl struct {
+	Config   map[string]interface{}
+	Postgres interface{} `path:"/postgres" method:"GET"`
 }
 
-func New(connStr string) *Healthchecker {
-	return &Healthchecker{ConnStr: connStr}
-}
-
-func (hc *Healthchecker) PostgresHealth(ctx echo.Context) error {
-	db, err := sql.Open("postgres", hc.ConnStr)
+func (hc *HealthchecksCtrl) PostgresFunc(ctx echo.Context) error {
+	db, err := sql.Open("postgres", hc.Config["PGConnStr"].(string))
 	if err != nil {
-		fmt.Println(err)
 		return ctx.String(http.StatusInternalServerError, "Connection error")
 	}
 	err = db.Ping()
 	if err != nil {
-		fmt.Println(err)
 		return ctx.String(http.StatusInternalServerError, "Connection error")
 	}
 	return ctx.String(http.StatusOK, "OK")
--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"os"
 
+	"github.com/nycdavid/ziptie"
 	"github.com/velvetreactor/postapocalypse/healthcheck"
 	"gopkg.in/labstack/echo.v3"
 )
 
 func main() {
 	e := echo.New()
-	pgConnStr := os.Getenv("PGCONN")
-	hc := healthcheck.New(pgConnStr)
-	e.GET("/healthcheck/postgres", hc.PostgresHealth)
+
+	// Controllers
+	healthchecksCtrl := &healthcheck.HealthchecksCtrl{
+		Config: map[string]interface{}{
+			"PGConnStr": os.Getenv("PGCONN"),
+		},
+	}
+
+	ziptie.Fasten(healthchecksCtrl, e)
+
+	// Start server
 	e.Logger.Fatal(e.Start(":3000"))
 }
