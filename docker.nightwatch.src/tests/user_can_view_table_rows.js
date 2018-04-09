9907735f3bdf5891e61b883fe69b34c068b668d1
David Ko <david.ko@velvetreactor.com>
2018-04-09 06:31:19 -0400

    Add failing integration test for pagination

--- docker.nightwatch.src/tests/user_can_view_table_rows.js
+++ docker.nightwatch.src/tests/user_can_view_table_rows.js
@@ -11,5 +11,18 @@ module.exports = {
     browser
       .waitForElementVisible('.table-rows tbody tr', 1000);
     browser.end();
+  },
+  'A User can click to page 2 in a table rows result set': browser => {
+    TestHelper.login(browser);
+    browser
+      .waitForElementVisible('.tables li', 1000)
+      .click('li.tables-items a');
+    browser
+      .click('ul.pages a[data-results-page="2"]');
+    browser.elements('css selector', '.table-rows tbody tr', qryObj => {
+      let els = qryObj.value;
+      browser.assert.equal(els.length, 8)
+    });
+    browser.end()
   }
 }
